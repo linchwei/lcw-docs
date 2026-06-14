8b5a99d88663856205fda79706a848bb86968e34
levy0802@qq.com <levy0802@qq.com>
2026-06-14 19:54:54 +0800

    chore(other): 🔨  remove vue version

--- apps/web-vue/src/utils/query-client.ts
+++
@@ -1,9 +0,0 @@
-import { QueryClient } from '@tanstack/vue-query'
-
-export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 30_000,
-        },
-    },
-})
--- packages/vue/src/composables/useEditorContentOrSelectionChange.ts
+++
@@ -1,9 +0,0 @@
-import type { LcwDocEditor } from '@lcw-doc/core'
-
-import { useEditorChange } from './useEditorChange'
-import { useEditorSelectionChange } from './useEditorSelectionChange'
-
-export function useEditorContentOrSelectionChange(callback: () => void, editor?: LcwDocEditor<any, any, any>) {
-    useEditorChange(callback, editor)
-    useEditorSelectionChange(callback, editor)
-}
